c76aa487a1f2699aec56961ca69898be424beed8
Johnny Tsai <johnny.tsai@iot-hmi.com>
2024-11-12 15:08:28 +0800

    en/3.4.3 workable version

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,5 +5,14 @@ $( document ).ready(function() {
     $(".ver_select").on( "change", function() {
         window.location.href = $(this).val();
     });
-    $("#site-nav").scrollTop($("a.nav-list-link.active").offset().top-60-($("#site-nav").height()/2));
+    var a = $("a.nav-list-link.active");
+    console.log("a:",a);
+    if (a) {
+        console.log("a.offset():",a.offset());
+        var b = a.offset();
+        console.log("b:",b);
+        if (b && b.top ) {
+            $("#site-nav").scrollTop(a.offset().top-60-($("#site-nav").height()/2));
+        }
+    }
 });
